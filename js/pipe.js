f54a98487ade3263be3a1a3b410448c75999bd6d
Marc Lopez Pujol <marclopez@MacBook-Pro-de-Marc-2.local>
2020-10-31 16:15:22 +0100

    Pipes.js & Player.js constructos and some methods done

--- js/pipe.js
+++ js/pipe.js
@@ -1,1 +1,29 @@
 "use strict"
+
+class Pipes{
+    constructor(canvas, y) {
+    this.pipeSize = {
+            widht: 50,
+            height: 200
+        }
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.pipePositionX = this.canvas.width;
+    this.pipePositionY = y;
+    this.speed = 5;
+    this.direction = -1;
+  }
+
+  move() {
+    this.x = this.x - this.speed;
+  }
+
+  draw() {
+    this.ctx.fillStyle = "red";
+    this.ctx.fillRect(this.x, this.y - this.pipeSize.height, this.pipeSize.widht, this.pipeSize.height);
+  }
+
+  setPipeSpeed(speed) {
+    this.speed = speed;
+  }
+}
--- js/player.js
+++ js/player.js
@@ -1,1 +1,41 @@
 "use strict"
+
+class Player {
+    constructor(canvas, vidas) {
+        this.canvas = canvas;
+        this.ctx = this.canvas.getContext("2d");
+        this.playerSize = {
+            widht: 50,
+            height: 50
+        }
+        this.speed = 5;
+        this.playerPositionY = 50;
+        this.playerPositionX = 100;
+        this.direction = 0;
+        this.vidas = vidas;
+    }
+
+    move() {
+        this.playerPositionY = this.playerPositionY + this.direction * this.speed;
+    }
+
+    draw() {
+        this.ctx.fillStyle = "green";
+      this.ctx.fillRect(
+      this.playerPositionX - this.playerSize.widht / 2,
+      this.playerPositionY - this.playerSize.height / 2,
+      this.size,
+      this.size
+    );
+    }
+
+    playerDirection(direction) {
+    this.direction = direction;
+    }
+    
+    loseLive() {
+    this.vidas--;
+  }
+
+
+}
