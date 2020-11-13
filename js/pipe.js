45fe4fcc02c39d7b27a7a8be2eac850ce3edbfb8
Marc Lopez Pujol <marclopez@MacBook-Pro-de-Marc-2.local>
2020-11-13 18:42:34 +0100

    porfin he añadido comentarios

--- js/pipe.js
+++ js/pipe.js
@@ -11,10 +11,10 @@ class Pipes{
       this.pipePositionX = this.canvas.width;
       this.pipePositionY = y;
       this.direction = -1;
-      this.entrePipes = false;
+      this.entrePipes = false; //condición que se asigna a cada pipe para comprobar que el pájaro ya ha pasado entre ellas
       this.gapPipe = 90;
       this.seconds = seconds;
-      this.pipesImages = {
+      this.pipesImages = { // assets de las pipes
         noche: {
           bottom: "./assets/img/pipes/pipered-botom.png",
           top: "./assets/img/pipes/pipered-top.png"
@@ -27,8 +27,7 @@ class Pipes{
   }
 
   move(speed) {
-    this.pipePositionX = this.pipePositionX - speed;
-    console.log(speed)
+    this.pipePositionX = this.pipePositionX - speed; //la temida función que hace el juego más difícil
   }
 
   draw(sueloImg, hora) {
@@ -38,7 +37,7 @@ class Pipes{
     this.pipeBottom.src =this.pipesImages[hora].bottom
     this.suelo = new Image();
     this.suelo.src = sueloImg[hora]
-    this.pipeYBottom = this.pipePositionY + this.pipeTop.height + this.gapPipe;
+    this.pipeYBottom = this.pipePositionY + this.pipeTop.height + this.gapPipe; //para pintar la pipe inferior debemos calcular su posción Y en base a donde colocamos la pipe superior + el gap
     this.pipeBottomY = this.pipeTopY + this.pipeSize.height + this.randomGap;
     this.ctx.drawImage(this.pipeTop, this.pipePositionX, this.pipePositionY);
     this.ctx.drawImage(this.pipeBottom, this.pipePositionX, this.pipeYBottom);
